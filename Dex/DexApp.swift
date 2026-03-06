69a3530f993b6504cc289207b020916dd295333d
JeanCamargoReal <jeancamargoreal@icloud.com>
2026-03-06 14:11:40 -0300

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,21 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Jean Camargo on 06/03/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
